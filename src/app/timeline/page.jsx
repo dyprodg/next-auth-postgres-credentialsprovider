3c5ae001614b7df341470c6d4743a55d3717182b
Dennis <dennisdiepolder@outlook.com>
2024-01-05 20:25:29 +0100

    useless function remove

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -3,7 +3,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import LogoutButton from "../components/LogoutButton";
-import { getActiveUser } from "@/lib/GetActiveUser";
+
 
 const Timeline = () => {
     const router = useRouter();
